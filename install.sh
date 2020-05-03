7aabe8fbf4cec709c7bb380f358b25fa7f30d637
yo6nam <32558308+yo6nam@users.noreply.github.com>
2020-05-03 19:33:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 cp rlabp.sh /opt/rolink/scripts && cp rlabp /etc/cron.d && cp /opt/rolink/conf/svxlink.conf /opt/rolink/conf/svxlinknorx.conf
 read -p "Press [Enter] to open the clone configuration for editing"
 sudo nano /opt/rolink/conf/svxlinknorx.conf
