974d3abb362f115d69884d48add9f0ccc85e1188
yo6nam <32558308+yo6nam@users.noreply.github.com>
2020-06-03 20:12:27 +0300

    Penalizare progresiva

--- rlabp.sh
+++ rlabp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Script anti abuz pe NET/RF & external trigger - YO6NAM
+# Script anti abuz pe NET/RF & external trigger & penalizare progresiva - YO6NAM
 
-# Set your limits below (bantime in seconds)
+# Set your limits below (ext_trig_btm sets the value in minutes for external trigger events)
 max_rf_ptt=4
 max_net_ptt=10
-bantime=1800
 reflector=reflector.439100.ro,rolink.rolink-net.ro,svx.dstar-yo.ro
+ext_trig_btm=10
 
 # Begin, nothing to edit below
 rf_ptt_bc=$(tail -22 /tmp/svxlink.log | grep -c "OPEN")
@@ -15,6 +15,13 @@ rf_ptt_bt=$(awk -v d1="$(date --date="-20 sec" "+%Y-%m-%d %H:%M:%S:")" \
 net_ptt=$(awk -v d1="$(date --date="-30 sec" "+%Y-%m-%d %H:%M:%S:")" \
 -v d2="$(date "+%Y-%m-%d %H:%M:%S:")" '$0 > d1 && $0 < d2 || $0 ~ d2' \
 /tmp/svxlink.log | grep -c "Talker stop")
+
+# Progressive penalty timer
+if [ ! -f /tmp/rlpt ]; then
+    echo "1" > /tmp/rlpt
+fi
+
+bantime=$(($(cat /tmp/rlpt) * 60))
 
 if [ $rf_ptt_bc -gt $max_rf_ptt ]; then
 	abuse=$(($rf_ptt_bc));
@@ -34,6 +41,7 @@ elif [ "$1" = "3" ]; then
 	logger -p User.alert "External trigger, blocking traffic."
 	touch /tmp/rolink.flg
 	sudo /sbin/iptables -I INPUT -s $reflector -j DROP
+	echo $ext_trig_btm > /tmp/rlpt
 	/opt/rolink/rolink-start.sh
 	exit 1
 elif [ "$1" = "2" ]; then
@@ -45,6 +53,7 @@ elif [ "$1" = "2" ]; then
 	exit 1
 elif [ "$1" = "1" ]; then
 	logger -p User.alert "External trigger, switching to TX only mode."
+	echo $ext_trig_btm > /tmp/rlpt
 	[ "$(pidof svxlink)" != "" ] && killall -v svxlink && sleep 1
 	/opt/rolink/bin/svxlink --daemon --config=/opt/rolink/conf/svxlinknorx.conf --logfile=/tmp/svxlink.log \
 	--runasuser=svxlink --pidfile=/var/run/svxlink.pid
@@ -60,6 +69,7 @@ fi
 
 if [ $abuse ]; then
 	logger -p User.alert "Abuse from RF detected ($abuse PTTs within 20 seconds)."
+	echo $(($(cat /tmp/rlpt) + 5 )) > /tmp/rlpt
 	[ "$(pidof svxlink)" != "" ] && killall -v svxlink && sleep 3
 	/opt/rolink/bin/svxlink --daemon --config=/opt/rolink/conf/svxlinknorx.conf --logfile=/tmp/svxlink.log \
 	--runasuser=svxlink --pidfile=/var/run/svxlink.pid
@@ -80,6 +90,6 @@ if [ -f /tmp/rolink.flg ] && [ "$(( $(date +"%s") - $(stat -c "%Y" /tmp/rolink.f
 	/opt/rolink/scripts/rolink-start.sh
 fi
 
-if [ $net_ptt -gt 2 ]; then
-	logger "ABP Net $net_ptt"
+if [ -f /tmp/rlpt ] && [ "$(( $(date +"%s") - $(stat -c "%Y" /tmp/rlpt) ))" -gt 3600 ]; then
+	echo "1" > /tmp/rlpt
 fi
